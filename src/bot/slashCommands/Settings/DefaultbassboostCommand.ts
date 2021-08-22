43c670a8f10124e0cedff369296dd6e5d5139709
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-22 21:28:01 +0200

    added default bassboost

---
+++ src/bot/slashCommands/Settings/DefaultbassboostCommand.ts
@@ -0,0 +1,41 @@
+import { SlashCommand } from "../../../client/structures/slashCommands";
+import { ApplyOptions } from "@sapphire/decorators";
+import { CommandInteraction } from "discord.js";
+import { bassboost } from "../../../client/constants/settings";
+
+@ApplyOptions<SlashCommand.Options>({
+	name: "defaultbassboost",
+	description: "Changes the default bassboost level for this server",
+	preconditions: ["GuildOnly"],
+	tDescription: "settings:defaultbass.description",
+	userPermissions: ["MANAGE_GUILD"],
+	arguments: [
+		{
+			name: "level",
+			description: "The level the default bassboost has to change to",
+			tDescription: "settings:defaultbass.args.level",
+			type: "STRING",
+			required: true,
+			choices: bassboost.map((str) => ({ name: str, value: str })),
+		},
+	],
+})
+export default class DefaultbassboostCommand extends SlashCommand {
+	public async run(interaction: CommandInteraction, args: SlashCommand.Args) {
+		if (!interaction.inGuild()) return;
+		await interaction.deferReply();
+
+		const defaultbassboost = args.getString("value", true);
+		const newConfig = await this.client.prisma.guild.update({
+			where: { id: interaction.guildId },
+			data: { defaultbassboost },
+		});
+		this.client.config.set(interaction.guildId, newConfig);
+
+		await interaction.followUp(
+			this.languageHandler.translate(interaction.guildId, "settings:defaultbass.success", {
+				defaultbassboost,
+			})
+		);
+	}
+}
