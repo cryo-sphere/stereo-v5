d11864016a030831021e051f908500089ba90b63
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-03 18:13:46 +0200

    Feat(Api): added "cors" env

--- src/client/structures/Api/Api.ts
+++ src/client/structures/Api/Api.ts
@@ -18,7 +18,7 @@ export class Api {
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		this.logger = client.loggers.get("api")!;
 		this.server.use(
-			cors({ credentials: true, origin: ["http://localhost:3000"] }),
+			cors({ credentials: true, origin: ["http://localhost:3000", ...((process.env.CORS as string).split(" "))] }),
 			json(),
 			cookieParser(),
 			new AuthMiddleware(client).middleware
