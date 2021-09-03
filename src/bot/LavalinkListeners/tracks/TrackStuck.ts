93461f0170755f189efa8c932d61fb380bcac637
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-03 19:33:25 +0200

    Feat: added trackStuck event

---
+++ src/bot/LavalinkListeners/tracks/TrackStuck.ts
@@ -0,0 +1,20 @@
+import { ListenerOptions } from "@sapphire/framework";
+import { LavalinkListener } from "../../../client/structures/lavalinkListener";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Player } from "@stereo-bot/lavalink";
+
+@ApplyOptions<ListenerOptions>({ event: "trackStuck" })
+export default class trackStuckListener extends LavalinkListener {
+	public async run({ player }: { player: Player }): Promise<void> {
+		const guild = this.client.guilds.cache.get(player.guild);
+		if (!guild) return;
+
+		player.skip();
+		const channel = await this.client.utils.getChannel(player.channels.text ?? "");
+		if (!channel || !channel.isText()) return;
+
+		await channel
+			.send(this.client.languageHandler.translate(guild.id, "MusicGeneral:stuck"))
+			.catch(() => void 0);
+	}
+}
