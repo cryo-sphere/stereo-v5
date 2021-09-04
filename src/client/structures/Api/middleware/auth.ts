ef5e95e11595c77c9e2107eb4b6748a5385fd5d4
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-04 10:38:38 +0200

    Feat(AuthMiddleware): added token refresh

--- src/client/structures/Api/middleware/auth.ts
+++ src/client/structures/Api/middleware/auth.ts
@@ -10,14 +10,32 @@ export class AuthMiddleware {
 		this.utils = new Utils(client);
 	}
 
-	private run(req: Request, res: Response, next: NextFunction) {
+	private async run(req: Request, res: Response, next: NextFunction) {
 		const authorization = req.cookies[this.cookieName];
 		if (authorization) {
 			req.auth = this.utils.decrypt(authorization);
 			if (!req.auth) res.clearCookie(this.cookieName);
 
-			if (req.auth && req.auth.expires < Date.now()) req.auth = null;
-			// to do: refresh token
+			if (req.auth && req.auth.expires < Date.now()) {
+				const data = await this.utils.getToken(req.auth.refresh, true);
+				if (data.error) {
+					req.auth = null;
+					return next();
+				}
+
+				const expires = Date.now() + data.expires_in * 1e3;
+				const raw = {
+					expires,
+					refresh: data.refresh_token,
+					token: data.access_token,
+					userId: req.auth.userId,
+				};
+
+				req.auth = raw;
+
+				const cookie = this.utils.encrypt(raw);
+				res.cookie("STEREO_AUTH", cookie, { maxAge: expires + data.expires_in * 1e3 });
+			}
 		} else {
 			req.auth = null;
 		}
