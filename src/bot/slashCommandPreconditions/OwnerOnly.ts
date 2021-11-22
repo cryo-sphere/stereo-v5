6285327e85365f822135b37510b90ad6efdb9ee1
DaanGamesDG <daan@daangamesdg.xyz>
2021-11-22 14:52:15 +0100

    feat: add mising messages

--- src/bot/slashCommandPreconditions/OwnerOnly.ts
+++ src/bot/slashCommandPreconditions/OwnerOnly.ts
@@ -1,11 +1,13 @@
 import { SlashCommandPrecondition } from "../../client";
 import type { CommandInteraction } from "discord.js";
+import { emojis } from "../../client/constants";
 
 export default class extends SlashCommandPrecondition {
 	public run(interaction: CommandInteraction): SlashCommandPrecondition.Result {
 		return this.client.owners.includes(interaction.user.id)
 			? this.ok()
 			: this.error({
+					message: `>>> ${emojis.redcross} | Only bot developers of **${this.client.user?.tag}** are allowed to use this command!`,
 					identifier: "BotGeneral:owneronly",
 					context: { bot: this.client.user?.tag }
 			  });
