fefeba051207f95b741ccf270b0525d8bc710a2b
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-06 21:40:02 +0100

    fix: making command error handlers compatible with new version

--- src/bot/listeners/commands/CommandDenied.ts
+++ src/bot/listeners/commands/CommandDenied.ts
@@ -1,13 +1,16 @@
-import type { CommandDeniedPayload, UserError } from "@sapphire/framework";
+import type { MessageCommandDeniedPayload, ChatInputCommandDeniedPayload, UserError, ContextMenuCommandDeniedPayload } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener } from "../../../client";
 
+type DeniedPayload = MessageCommandDeniedPayload | ChatInputCommandDeniedPayload | ContextMenuCommandDeniedPayload;
+
 @ApplyOptions<Listener.Options>({ once: false, event: "commandDenied" })
 export default class extends Listener {
-	public async run({ context, message: content }: UserError, { message }: CommandDeniedPayload) {
+	public run({ context, message: content }: UserError, payload: DeniedPayload) {
 		if (Reflect.get(Object(context), "silent")) return;
 
-		return message.reply({
+		const ctx = "message" in payload ? payload.message : payload.interaction;
+		return ctx.reply({
 			content,
 			allowedMentions: { repliedUser: true }
 		});
--- src/bot/listeners/commands/CommandError.ts
+++ src/bot/listeners/commands/CommandError.ts
@@ -1,6 +1,13 @@
-import { Args, ArgumentError, CommandErrorPayload, Events, UserError } from "@sapphire/framework";
-import { DiscordAPIError, HTTPError, Message } from "discord.js";
-import { Command, Listener } from "../../../client/";
+import {
+	ArgumentError,
+	ChatInputCommandErrorPayload,
+	ContextMenuCommandErrorPayload,
+	Events,
+	MessageCommandErrorPayload,
+	UserError
+} from "@sapphire/framework";
+import { DiscordAPIError, HTTPError } from "discord.js";
+import { Listener } from "../../../client/";
 import { RESTJSONErrorCodes } from "discord-api-types/v9";
 import { ApplyOptions } from "@sapphire/decorators";
 import { codeBlock } from "@sapphire/utilities";
@@ -8,37 +15,45 @@ import { emojis } from "../../../client/constants";
 
 const ignoredCodes = [RESTJSONErrorCodes.UnknownChannel, RESTJSONErrorCodes.UnknownMessage];
 
+type ErrorPayload = MessageCommandErrorPayload | ChatInputCommandErrorPayload | ContextMenuCommandErrorPayload;
+
 @ApplyOptions<Listener.Options>({ event: "commandError" })
 export default class extends Listener {
-	public async run(error: Error, { message, piece, args }: CommandErrorPayload) {
+	public run(error: Error, payload: ErrorPayload) {
+		const ctx = "message" in payload ? payload.message : payload.interaction;
+		const author = "user" in ctx ? ctx.user.id : ctx.author.id;
 		const errorEmoji = emojis.error;
 
 		// If string || UserError, send to user
-		if (typeof error === "string") return message.reply(`>>> ${errorEmoji} | ${error}`);
-		if (error instanceof ArgumentError) return message.reply(`>>> ${errorEmoji} | ${error.message}`);
-		if (error instanceof UserError) return message.reply(`>>> ${errorEmoji} | ${error.message}`);
+		if (typeof error === "string") return ctx.reply(`>>> ${errorEmoji} | ${error}`);
+		if (error instanceof ArgumentError) return ctx.reply(`>>> ${errorEmoji} | ${error.message}`);
+		if (error instanceof UserError) return ctx.reply(`>>> ${errorEmoji} | ${error.message}`);
 
 		if (error.name === "AbortError" || error.message === "Internal Server Error") {
-			this.logger.warn(`${this.getWarnError(message)} (${message.author.id}) | ${error.constructor.name} | ${error.message}`);
+			this.logger.warn(
+				`${this.getWarnError(author, ctx.id, ctx.channelId, ctx.guildId)} (${author}) | ${error.constructor.name} | ${error.message}`
+			);
 
-			return message.reply(
+			return ctx.reply(
 				`>>> ${errorEmoji} | Oh no, this doesn't look very good. Something caused the request to abort their mission, please try again.`
 			);
 		}
 
 		// checks if error is DiscordAPIError || HTTPError
 		if (error instanceof DiscordAPIError || error instanceof HTTPError) {
-			if (this.isSilencedError(args, error)) return;
+			if (this.isSilencedError(ctx.channelId, ctx.guildId, error)) return;
 			this.container.client.emit("error", error);
 		} else {
-			this.logger.warn(`${this.getWarnError(message)} (${message.author.id}) | ${error.constructor.name} | ${error.message}`);
+			this.logger.warn(
+				`${this.getWarnError(author, ctx.id, ctx.channelId, ctx.guildId)} (${author}) | ${error.constructor.name} | ${error.message}`
+			);
 		}
 
-		const command = piece as Command;
+		const { command } = payload;
 		this.logger.fatal(`[COMMAND] ${command.location.relative}\n${error.stack || error.message}`);
 
 		try {
-			return message.reply(this.generateUnexpectedErrorMessage(args, error));
+			return ctx.reply(this.generateUnexpectedErrorMessage(author, error));
 		} catch (err) {
 			this.container.client.emit(Events.Error, err);
 		}
@@ -46,23 +61,23 @@ export default class extends Listener {
 		return undefined;
 	}
 
-	private isSilencedError(args: Args, error: DiscordAPIError | HTTPError) {
-		return ignoredCodes.includes(error.code) || this.isDirectMessageReplyAfterBlock(args, error);
+	private isSilencedError(channelId: string, guild: string | null, error: DiscordAPIError | HTTPError) {
+		return ignoredCodes.includes(error.code) || this.isDirectMessageReplyAfterBlock(channelId, guild, error);
 	}
 
-	private isDirectMessageReplyAfterBlock(args: Args, error: DiscordAPIError | HTTPError) {
+	private isDirectMessageReplyAfterBlock(channelId: string, guild: string | null, error: DiscordAPIError | HTTPError) {
 		if (error.code !== RESTJSONErrorCodes.CannotSendMessagesToThisUser) return false;
-		if (args.message.guild !== null) return false;
-		return error.path === `/channels/${args.message.channel.id}/messages`;
+		if (guild !== null) return false;
+		return error.path === `/channels/${channelId}/messages`;
 	}
 
-	private generateUnexpectedErrorMessage(args: Args, error: Error) {
-		if (this.container.client.owners.includes(args.message.author.id)) return codeBlock("js", error.stack ?? error.message);
+	private generateUnexpectedErrorMessage(authorId: string, error: Error) {
+		if (this.container.client.owners.includes(authorId)) return codeBlock("js", error.stack ?? error.message);
 
 		return `>>> ${emojis.error} | Oh no, this doesn't look very good.\n**Error**: \`${error.message}\`\nIf this keeps happening, please DM the developer of this bot.`;
 	}
 
-	private getWarnError(message: Message) {
-		return `ERROR: /${message.guild ? `${message.guild.id}/${message.channel.id}` : `DM/${message.author.id}`}/${message.id}`;
+	private getWarnError(author: string, id: string, channelId?: string, guildId?: string | null) {
+		return `ERROR: /${guildId ? `${guildId}/${channelId}` : `DM/${author}`}/${id}`;
 	}
 }
