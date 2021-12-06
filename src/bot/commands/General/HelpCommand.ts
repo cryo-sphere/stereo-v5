c210585b06802b69443872a79b94a5f5816989cb
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-06 21:31:58 +0100

    fix: marking .husky files as exe files

--- src/bot/commands/General/HelpCommand.ts
+++ src/bot/commands/General/HelpCommand.ts
@@ -1,6 +1,6 @@
 import { Command } from "../../../client/";
 import { ApplyOptions } from "@sapphire/decorators";
-import type { EmbedFieldData, Message, MessageEmbed } from "discord.js";
+import type { CommandInteraction, EmbedFieldData, Message, MessageEmbed } from "discord.js";
 import ms from "ms";
 
 @ApplyOptions<Command.Options>({
@@ -11,7 +11,7 @@ import ms from "ms";
 	usage: "[command]"
 })
 export default class extends Command {
-	public async messageRun(message: Message, args: Command.Args, context: Command.Context): Promise<void> {
+	public async messageRun(message: Message, args: Command.Args, context: Command.MessageContext): Promise<void> {
 		const embed: MessageEmbed = this.client.utils
 			.embed()
 			.setTitle(`Help Command - ${message.author.tag}`)
@@ -58,5 +58,56 @@ export default class extends Command {
 		}
 
 		await message.reply({ embeds: [embed] });
+	}
+
+	public async chatInputRun(interaction: CommandInteraction, context: Command.SlashCommandContext) {
+		const embed = this.client.utils
+			.embed()
+			.setTitle(`Help Command - ${interaction.user.tag}`)
+			.setFooter("Bot created by DaanGamesDG#7621", "https://static.daangamesdg.xyz/discord/pfp.gif");
+
+		const cmd = interaction.options.getString("command", false);
+		const command = this.container.stores.get("commands").get(cmd ?? "") as Command | undefined;
+
+		if (command) {
+			const userPermissions = this.client.utils.formatPerms(command.permissions);
+			const clientPermissions = this.client.utils.formatPerms(command.clientPermissions);
+
+			embed.setDescription(
+				[
+					`>>> 🏷 | **Name**: ${command.name}`,
+					`📁 | **Category**: ${command.category}`,
+					`🔖 | **Aliases**: \`${command.aliases.join("`, `") || "-"}\`\n`,
+					`📋 | **Usage**: ${command.usage ? `${context.commandPrefix}${command.usage}` : "-"}`,
+					`📘 | **Description**: ${command.description ?? "-"}\n`,
+					`👮‍♂️ | **User Permissions**: ${userPermissions ?? "-"}`,
+					`🤖 | **Client Permissions**: ${clientPermissions ?? "-"}`,
+					`⌚ | **Cooldown**: \`${ms(command.cooldown, { long: false })}\``,
+					`🔢 | **Cooldown Limit**: \`${command.cooldownLimit}\``
+				].join("\n")
+			);
+		} else {
+			const isOwner = this.client.isOwner(interaction.user.id);
+			const commands = [...this.container.stores.get("commands").values()] as Command[];
+			let categories = [...new Set(commands.map((c) => c.category ?? "default"))];
+
+			if (!isOwner) categories = categories.filter((c) => c.toLowerCase() !== "developers");
+
+			const fields: EmbedFieldData[] = categories.map((category) => {
+				const valid = commands.filter((c) => c.category === category);
+				const filtered = isOwner ? valid : valid.filter((c) => !c.hidden || !c.OwnerOnly);
+
+				return {
+					name: `• ${category}`,
+					value: filtered.map((c) => `\`${c.name ?? c.aliases[0] ?? "unkown"}\``).join(" ")
+				};
+			});
+
+			embed.setFields(fields);
+		}
+
+		await interaction.reply({
+			embeds: [embed]
+		});
 	}
 }
