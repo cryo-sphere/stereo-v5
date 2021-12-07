a0e9856995cc152a1bd15c034dff0960b64ac87e
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-07 21:41:22 +0100

    feat(PingCommand): add messageCommand input

--- src/bot/commands/General/PingCommand.ts
+++ src/bot/commands/General/PingCommand.ts
@@ -1,12 +1,16 @@
 import { Command } from "../../../client/";
 import { ApplyOptions } from "@sapphire/decorators";
-import type { Message } from "discord.js";
+import type { CommandInteraction, Message } from "discord.js";
 import ms from "ms";
 
 @ApplyOptions<Command.Options>({
 	name: "ping",
 	aliases: ["pong"],
-	description: "Ping! Pong! 🏓"
+	description: "Ping! Pong! 🏓",
+	chatInputCommand: {
+		register: true,
+		messageCommand: true
+	}
 })
 export default class extends Command {
 	public async messageRun(message: Message): Promise<void> {
@@ -22,6 +26,30 @@ export default class extends Command {
 						[
 							`API Latency: \`${this.client.ws.ping}\` ms`,
 							`Edit Latency: \`${msg.createdTimestamp - message.createdTimestamp}\` ms`,
+							`Uptime: \`${ms(this.client.uptime ?? 0, {
+								long: true
+							})}\``
+						].join("\n")
+					)
+			]
+		});
+	}
+
+	public async chatInputRun(interaction: CommandInteraction) {
+		const interactionDate = Date.now();
+		await interaction.reply(">>> 🏓 | Pinging...");
+		const date = Date.now();
+
+		await interaction.editReply({
+			content: null,
+			embeds: [
+				this.client.utils
+					.embed()
+					.setTitle("🏓 Pong!")
+					.setDescription(
+						[
+							`API Latency: \`${this.client.ws.ping}\` ms`,
+							`Edit Latency: \`${date - interactionDate}\` ms`,
 							`Uptime: \`${ms(this.client.uptime ?? 0, {
 								long: true
 							})}\``
