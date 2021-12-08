b039ebee6c765de0500d490ffca629e83303bc4e
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-08 14:27:18 +0100

    feat(ExecCommand): make command compatible with interactions

--- src/bot/commands/Developers/ExecCommand.ts
+++ src/bot/commands/Developers/ExecCommand.ts
@@ -2,14 +2,32 @@ import { Command } from "../../../client/";
 import { ApplyOptions } from "@sapphire/decorators";
 import { exec, ExecException } from "child_process";
 import { codeBlock } from "@sapphire/utilities";
-import type { Message } from "discord.js";
+import type { CommandInteraction, Message } from "discord.js";
 
 @ApplyOptions<Command.Options>({
 	name: "exec",
 	aliases: ["execute", "terminal"],
 	description: "Execute any command using exec",
 	preconditions: ["OwnerOnly"],
-	usage: "<...command>"
+	usage: "<...command>",
+	chatInputCommand: {
+		register: true,
+		messageCommand: true,
+		options: [
+			{
+				name: "command",
+				description: "Command to execute",
+				type: "STRING",
+				required: true
+			},
+			{
+				name: "silent",
+				description: "If the response should be silent",
+				type: "BOOLEAN",
+				required: false
+			}
+		]
+	}
 })
 export default class extends Command {
 	public async messageRun(message: Message, args: Command.Args) {
@@ -25,6 +43,24 @@ export default class extends Command {
 			});
 
 		return message.reply(`${output}`);
+	}
+
+	public async chatInputRun(interaction: CommandInteraction) {
+		const command = interaction.options.getString("command", true);
+		const silent = interaction.options.getBoolean("silent") ?? false;
+		await interaction.deferReply({ ephemeral: silent });
+
+		const { result, success } = await this.exec(command);
+		const output = success ? codeBlock("bash", result) : `**Error**: ${codeBlock("bash", result)}`;
+		if (silent) return interaction.followUp({ content: "Executed!", ephemeral: true });
+
+		if (output.length > 2000)
+			return interaction.followUp({
+				files: [{ attachment: Buffer.from(output), name: "output.txt" }],
+				content: "Output was too long... sent the result as a file."
+			});
+
+		return interaction.followUp(`${output}`);
 	}
 
 	private async exec(command: string) {
