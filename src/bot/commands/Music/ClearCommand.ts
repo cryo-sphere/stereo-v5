29315d5e9bd39db20a84ce79c1a984bc70cb4d84
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-17 18:55:32 +0100

    feat: add clear command

---
+++ src/bot/commands/Music/ClearCommand.ts
@@ -0,0 +1,53 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import type { CommandInteraction, VoiceChannel } from "discord.js";
+import { Command } from "../../../client";
+
+@ApplyOptions<Command.Options>({
+	name: "clear",
+	preconditions: ["GuildOnly"],
+	description: "Clears the queue (except the current song)",
+	tDescription: "music:clear.description",
+	musicPermissions: ["EDIT_QUEUE"],
+	chatInputCommand: {
+		register: true,
+		messageCommand: true
+	}
+})
+export default class extends Command {
+	public async chatInputRun(interaction: CommandInteraction) {
+		if (!interaction.inGuild()) return;
+
+		const player = this.client.manager.get(interaction.guildId);
+		if (!player) {
+			await interaction.reply(this.translate.translate(interaction.guildId, "MusicGeneral:noPlayer"));
+			return;
+		}
+
+		const state = interaction.guild?.voiceStates.cache.get(interaction.user.id);
+		if (player.channels.voice && state?.channelId !== player.channels.voice) {
+			await interaction.deferReply();
+			const channel = (await this.client.utils.getChannel(player.channels.voice)) as VoiceChannel;
+			await interaction.followUp(
+				this.translate.translate(interaction.guildId, "MusicGeneral:vc.wrong", {
+					voice: channel.name
+				})
+			);
+
+			return;
+		}
+
+		if (!player.queue.current) {
+			await interaction.reply(this.translate.translate(interaction.guildId, "MusicGeneral:noTrack"));
+			return;
+		}
+
+		if (!player.queue.next.length) {
+			await interaction.reply(this.translate.translate(interaction.guildId, "MusicGeneral:noQueue"));
+			return;
+		}
+
+		player.queue.next = [];
+		player.queue.previous = [];
+		await interaction.reply(this.translate.translate(interaction.guildId, "music:clear.success"));
+	}
+}
